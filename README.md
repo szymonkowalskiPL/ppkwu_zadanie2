f7777d8fe5a9fc8758dda475daba9bba25fa9fd5
szkowalski <222477@edu.p.lodz.pl>
2021-10-28 12:33:02 +0200

    readme file updated, description of endpoint added

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 String API
+Check your string for how many uppercase letters, lowercase letters, numbers or special characters it has. Any combination is supported.  
+
+required packages:
+1.Flask (pip install flask)
+
+Endpoints:
+
+1. /checkstring (POST)
+	
+	Endpoint accepts string and return json with result of calculations
+
+	Arguments: "string" (type: string)
+	
+	Example of usage
+
+	request -> http://127.0.0.1:5000/checkstring?string=Szymon
+	
+	response ->{"upper_case": 1, 
+				"lower_case": 5, 
+				"numbers": 0, 
+				"special_characters": 0
+				}
--- rest-api.py
+++ rest-api.py
@@ -2,25 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/s',  methods=['GET', 'POST'])
-def calculateChecksum():
-    '''
+@app.route('/checkstring',  methods=['GET', 'POST'])
+def checkString():
+    
     string = request.args.get('string')
+    print(string)
     
-    md5hash = hashlib.md5(string.encode('utf-8')).hexdigest()
-
-    crc32hash = binascii.crc32(string.encode('utf8'))
-    
-    sha1hash = hashlib.sha1(string.encode('utf-8')).hexdigest()
-    
-    sha256hash = hashlib.sha256(string.encode('utf-8')).hexdigest()
-    
-    return jsonify(
-        md5=md5hash,
-        crc32=crc32hash,
-        sha1=sha1hash,
-        sha256=sha256hash,
-    )
-    '''
 
 app.run()
