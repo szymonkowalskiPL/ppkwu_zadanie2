a38a0e54f71b99cacf18b3f88a5ef28ac2cbc744
szkowalski <222477@edu.p.lodz.pl>
2021-10-28 12:49:36 +0200

    counting uppercase and lowercase letters in string added

--- rest-api.py
+++ rest-api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ def checkString():
     string = request.args.get('string')
     print(string)
 
+    upper =len(re.findall(r'[A-Z]',string))
+    lower =len(re.findall(r'[a-z]',string))
+    print(upper, lower)
+    
     return string
 
 app.run()
