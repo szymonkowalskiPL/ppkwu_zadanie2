102493508b688891de063bb048bc8d0d9e3225e5
szkowalski <222477@edu.p.lodz.pl>
2021-10-28 12:53:10 +0200

    counting numbers in string added

--- rest-api.py
+++ rest-api.py
@@ -9,9 +9,10 @@ def checkString():
     string = request.args.get('string')
     print(string)
 
-    upper =len(re.findall(r'[A-Z]',string))
-    lower =len(re.findall(r'[a-z]',string))
-    print(upper, lower)
+    upper = len(re.findall(r'[A-Z]',string))
+    lower = len(re.findall(r'[a-z]',string))
+    numbers = len(re.findall(r'[0-9]',string))
+    print(upper, lower, numbers)
     
     return string
 
